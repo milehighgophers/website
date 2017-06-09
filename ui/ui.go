be6209fdb424dfc6fa922f1a2ae3c99c58f9d11f
Eno Compton <eno4@ecom.com>
2017-06-09 15:25:50 -0600

    Extract shared templates from UI

--- ui/ui.go
+++ ui/ui.go
@@ -7,74 +7,12 @@ import (
 	"github.com/milehighgophers/website/data"
 )
 
-const (
-	indexTemplateStr = `
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Mile High Gopher Events</title>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
-		<link rel="stylesheet" href="/assets/styles.css">
-	</head>
-	<body>
-		<header class="header">
-			<img src="/assets/logo.png" alt="mile-high-gophers-logo" class="logo">
-			<img src="/assets/hero.jpg" alt="mountains-backdrop">
-		</header>
-		<section class="body flex-container">
-			<div class="flex-1">
-				<h1>Boulder</h1>
-				<ul>
-				{{range .BoulderEvents}}
-					<li>
-						<dl>
-							<dt>{{.HumanTime}}</dt>
-							<dd>{{.Name}}</dd>
-						</dl>
-					</li>
-				{{else}}
-					<div><strong>No Events</strong></div>
-				{{end}}
-				</ul>
-			</div>
-			<div class="flex-1">
-				<h1>Denver</h1>
-				<ul>
-				{{range .DenverEvents}}
-					<li>
-						<dl>
-							<dt>{{.HumanTime}}</dt>
-							<dd>{{.Name}}</dd>
-						</dl>
-					</li>
-				{{else}}
-					<div><strong>No Events</strong></div>
-				{{end}}
-				</ul>
-			</div>
-			<div class="flex-1">
-				<h1>Denver Tech Center</h1>
-				<ul>
-				{{range .DTCEvents}}
-					<li>
-						<dl>
-							<dt>{{.HumanTime}}</dt>
-							<dd>{{.Name}}</dd>
-						</dl>
-					</li>
-				{{else}}
-					<div><strong>No Events</strong></div>
-				{{end}}
-				</ul>
-			</div>
-		</section>
-	</body>
-</html>
-`
+var indexTemplate = template.Must(
+	template.ParseFiles(
+		"ui/templates/index.html",
+		"ui/templates/events.html",
+	),
 )
-
-var indexTemplate = template.Must(template.New("index").Parse(indexTemplateStr))
 
 // Render will turn meetup event data into something to write out.
 func Render(s *data.MeetupSchedule) []byte {
