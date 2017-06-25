68756e8323a83e2cf5c51e9549e4e586cfa00552
Jason Keene <jasonkeene@gmail.com>
2017-06-25 15:12:08 -0600

    Include template files with binary

--- ui/ui.go
+++ ui/ui.go
@@ -3,18 +3,29 @@ package ui
 import (
 	"bytes"
 	"html/template"
+	"log"
 
 	"github.com/milehighgophers/website/data"
 )
 
-//go:generate go-bindata -pkg ui assets
+//go:generate go-bindata -pkg ui assets templates
 
-var indexTemplate = template.Must(
-	template.ParseFiles(
-		"ui/templates/index.html",
-		"ui/templates/events.html",
-	),
-)
+var indexTemplate *template.Template
+
+func init() {
+	t := template.New("index")
+	events, err := Asset("templates/events.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	index, err := Asset("templates/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t = template.Must(t.Parse(string(index)))
+	t = template.Must(t.Parse(string(events)))
+	indexTemplate = t
+}
 
 // Render will turn meetup event data into something to write out.
 func Render(s *data.MeetupSchedule) []byte {
